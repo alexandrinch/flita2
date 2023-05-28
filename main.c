c1bc5366ae4da2d6696a386661f76173c1aab845
Alexander <114886944+alexandrinch@users.noreply.github.com>
2023-05-28 22:25:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,19 @@
 int main(int argc, char *argv[]) {
     struct graph graph = {0};
     if(argc != 2){
-        puts("Error input! Enter path to file.");
+        puts("Enter path to file.");
         return 1;
     }
     FILE* file = fopen(argv[1], "r");
     if (file == NULL) {
-        fprintf(stderr, "Error. Unable to open file.\n");
+        fprintf(stderr, "Unable to open file.\n");
         return 1;
     }
     read_graph(file, &graph);
     fclose(file);
     file = fopen("graph.dot", "w");
     if (file == NULL) {
-        fprintf(stderr, "Error. Unable to open file.\n");
+        fprintf(stderr, "Unable to open file.\n");
         return 1;
     }
     write_dot(file, &graph);
